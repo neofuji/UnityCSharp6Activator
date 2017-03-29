1b88e7708805d5c156715c9e3b7959120d0fbf4a
neofuji <shizuruma@outlook.jp>
2017-03-30 02:49:16 +0900

    Add Assets

---
+++ Assets/Editor/CSharp6Activator.cs
@@ -0,0 +1,38 @@
+﻿using System.IO;
+using System.Text;
+using System.Xml.Linq;
+using UnityEditor;
+using SyntaxTree.VisualStudio.Unity.Bridge;
+
+[InitializeOnLoad]
+static class CSharp6Activator
+{
+    sealed class Utf8StringWriter : StringWriter
+    {
+        public override Encoding Encoding => Encoding.UTF8;
+    }
+
+    static CSharp6Activator()
+    {
+        ProjectFilesGenerator.ProjectFileGeneration += (name, content) =>
+        {
+            XNamespace ns = "http://schemas.microsoft.com/developer/msbuild/2003";
+
+            var document = XDocument.Parse(content);
+
+            foreach (var element in document.Root.Elements(ns + "PropertyGroup"))
+            {
+                foreach (var elem in element.Elements(ns + "LangVersion"))
+                {
+                    elem.Value = "6";
+                }
+            }
+
+            using (var sw = new Utf8StringWriter())
+            {
+                document.Save(sw);
+                return sw.ToString();
+            }
+        };
+    }
+}
